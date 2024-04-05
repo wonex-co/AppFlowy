c681eb254bcb21efca3b4a312c9909d09173dd97
Abel Chiquetti <77760712+AbelChiquetti@users.noreply.github.com>
2024-04-05 20:38:16 -0300

    Update Name

--- frontend/appflowy_flutter/windows/runner/main.cpp
+++ frontend/appflowy_flutter/windows/runner/main.cpp
@@ -7,15 +7,15 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
-  HANDLE hMutexInstance = CreateMutex(NULL, TRUE, L"AppFlowyMutex");
-  HWND handle = FindWindowA(NULL, "AppFlowy");
+  HANDLE hMutexInstance = CreateMutex(NULL, TRUE, L"REKREIOMutex");
+  HWND handle = FindWindowA(NULL, "REKREIO");
 
   if (GetLastError() == ERROR_ALREADY_EXISTS) {
     flutter::DartProject project(L"data");
     std::vector<std::string> command_line_arguments = GetCommandLineArguments();
     project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
     FlutterWindow window(project);
-    if (window.SendAppLinkToInstance(L"AppFlowy")) {
+    if (window.SendAppLinkToInstance(L"REKREIO")) {
       return false;
     }
 
@@ -44,7 +44,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"AppFlowy", origin, size)) {
+  if (!window.CreateAndShow(L"REKREIO", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
